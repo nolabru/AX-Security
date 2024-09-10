bd429b476b8d91092fd2402ee5f718b316f49706
Bruno Nolasco <139811133+nolabru@users.noreply.github.com>
2024-09-10 00:05:22 -0300

    up

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Toggle Menu ---------------------------
+
+const menu = document.getElementById('toggleMenu');
+
+menu.addEventListener('click', () =>{
+
+    menu.classLista.add('active');
+
+});
