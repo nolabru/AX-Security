791f524d11f7d50b15fc78047fca072dea3a0088
Bruno Nolasco <139811133+nolabru@users.noreply.github.com>
2024-09-12 00:06:39 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@
 function menuT(){
     let menuToggle = document.querySelector('.toggleMenu');
     let navBar = document.getElementById('navBarLinks');
+    let menuToggleFooter = document.querySelector('.toggleFooter')
+    let navBarFooter = document.getElementById('navFooterLinks');
     menuToggle.classList.toggle('active');
+    menuToggleFooter.classList.toggle('active');
     navBar.classList.toggle('active');
+    navBarFooter.classList.toggle('active');
 }
