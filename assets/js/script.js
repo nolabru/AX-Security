523474fd594dac2af2d7e8403fbefc0487005f47
Bruno Nolasco <139811133+nolabru@users.noreply.github.com>
2024-09-13 00:31:27 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,30 @@ function menuT(){
     let navBar = document.getElementById('navBarLinks');
     let menuToggleFooter = document.querySelector('.toggleFooter')
     let navBarFooter = document.getElementById('navFooterLinks');
-    menuToggle.classList.toggle('active');
-    menuToggleFooter.classList.toggle('active');
-    navBar.classList.toggle('active');
-    navBarFooter.classList.toggle('active');
+
+    const toggles = [menuToggle, menuToggleFooter, navBar, navBarFooter];
+    let toggle = 0;
+    for(toggle in toggles){
+        console.log(toggles[toggle]);
+        toggles[toggle].classList.toggle('active');
+        toggle ++;
+    }   
+}
+
+// Detalhes Serviços ---------------------
+
+const detalheSeg = document.querySelector('.svcBtn.seg');
+const detalheBox = document.querySelector('.svcDetalhes');
+const body = document.querySelector('body');
+const toggleDetalhes = document.querySelector('.toggleDetalhes');
+detalheSeg.addEventListener('click', segDt);
+
+function segDt(){
+    detalheBox.classList.add('active');
+    body.classList.add('active');
+    toggleDetalhes.addEventListener('click', back);
+    function back(){
+        detalheBox.classList.remove('active');
+        body.classList.remove('active');
+    }
 }
