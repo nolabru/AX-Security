203fa3ef7b87742369ec9e207a429c24fece6714
Bruno Nolasco <139811133+nolabru@users.noreply.github.com>
2024-09-15 23:13:45 -0300

    up

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -1,8 +1,8 @@
 <?php
   //Variáveis
   $email = $_POST['email'];
-  $telefone = $_POST['telefone']
-  $assunto = $_POST['assunto']
+  $telefone = $_POST['telefone'];
+  $assunto = $_POST['assunto'];
   $mensagem = $_POST['mensagem'];
 
   $data_envio = date('d/m/Y');
@@ -18,17 +18,17 @@
         <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
     </html>
   ";
-  
+
   //Emails para quem será enviado o formulário
   $destino = "bruno2403moreira@gmail.com";
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
   $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  $headers .= "Content-type: text/html; charset=utf-8\n";
+  $headers .= "From: $email";
 
   //Enviar
-  mail($destino,$assunto, $arquivo, $headers);
+  mail($destino, $assunto, $arquivo, $headers);
   
   echo "E-mail Enviado com Sucesso!";
 ?>
