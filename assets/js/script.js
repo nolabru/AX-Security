6d3e4bc0f69f22366faaaf50638e30b1e50d8c4a
Bruno Nolasco <139811133+nolabru@users.noreply.github.com>
2024-09-19 21:18:39 -0300

    Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 function openIg() {
     window.open("https://www.instagram.com/axsecurity", "_blank", 'noopener, noteferrez');
 }
+
+let copy = document.querySelector('.sliderClientes').cloneNode(true);
+document.querySelector('.clientes').appendChild(copy);
